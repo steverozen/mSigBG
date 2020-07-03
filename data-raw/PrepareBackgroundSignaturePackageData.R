29efe48d45f116f195499e67353df89a6c8d0eb4
steverozen <steve@steverozen.net>
2020-07-03 12:33:17 +0800

    more cleanup

--- data-raw/PrepareBackgroundSignaturePackageData.R
+++ data-raw/PrepareBackgroundSignaturePackageData.R
@@ -1,16 +1,37 @@
-# To create background information variables, at top level, run
-# the following functions
-# 
-# MakeMCF10HepG2BackgroundVars()
-# SaveHepG2andMCF10ABackgroundInfo()
-
-#' Create spectra from VCFs and load spectra as package variables.
-#' VCFs received from Arnoud on 2020 Feb 04
-#' 
-#' @keywords internal
 
 
-MakeMCF10HepG2BackgroundVars <- function() {
+# Run this to create package variables HepG2.background.spectra
+# and MCF10A.background.spectra
+HepG2.background.spectra <- ICAMS::ReadCatalog(
+  "data-raw/package.variable.source.data/Background_HepG2.csv",
+  region = "genome",
+  catalog.type = "counts")
+attr(HepG2.background.spectra, "abundance") <- 
+  ICAMS::all.abundance$BSgenome.Hsapiens.1000genomes.hs37d5$genome$"96"
+usethis::use_data(HepG2.background.spectra, overwrite = TRUE)
+
+
+MCF10A.background.spectra <- ICAMS::ReadCatalog(
+  "data-raw/package.variable.source.data/Background_MCF10A.csv",
+  region = "genome",
+  catalog.type = "counts")
+attr(MCF10A.background.spectra, "abundance") <- 
+  ICAMS::all.abundance$BSgenome.Hsapiens.1000genomes.hs37d5$genome$"96"
+usethis::use_data(MCF10A.background.spectra, overwrite = TRUE)
+                          
+
+# Run this code to create the package variables HepG2.background
+# and MCF10A.background.
+HepG2.background.info <- 
+  MakeBackgroundInfo(mSigBG::HepG2.background.spectra, title = "HepG2.background")
+MCF10A.background.info <- 
+  MakeBackgroundInfo(mSigBG::MCF10A.background.spectra, title = "MCF10A.background")
+usethis::use_data(HepG2.background.info, overwrite = TRUE)
+usethis::use_data(MCF10A.background.info, overwrite = TRUE)
+
+
+
+OLD.MakeMCF10HepG2BackgroundVars <- function() {
   data.dir <- file.path("data-raw",
                         "background.signature.spectra", 
                         "backgroundVCFS-2020-02-04")
@@ -33,21 +54,9 @@ MakeMCF10HepG2BackgroundVars <- function() {
   
   # The ref genomes have an arbitrary file path encoded in them
   attr(sbs.cat$catSBS96, "ref.genome") <- NULL
-
+  
   HepG2.background.spectra  <- sbs.cat$catSBS96[ , 1:3]
   MCF10A.background.spectra <- sbs.cat$catSBS96[ , 4:6]
   usethis::use_data(HepG2.background.spectra, overwrite = TRUE)
   usethis::use_data(MCF10A.background.spectra, overwrite = TRUE)
-}
-
-
-# Run this code to create the package variables HepG2.background
-# and MCF10A.background.
-if (FALSE) {
-  HepG2.background.info <- 
-    MakeBackgroundInfo(mSigBG::HepG2.background.spectra, title = "HepG2.background")
-  MCF10A.background.info <- 
-    MakeBackgroundInfo(mSigBG::MCF10A.background.spectra, title = "MCF10A.background")
-  usethis::use_data(HepG2.background.info, overwrite = TRUE)
-  usethis::use_data(MCF10A.background.info, overwrite = TRUE)
 }
