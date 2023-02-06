14d6f9d1f86c499a5e5532f50620edade3a8bbd2
Steven G. Rozen <steve@steverozen.net>
2023-02-06 18:48:04 +0800

    edit to the platinuRMD

--- R/subcolor_sim.R
+++ R/subcolor_sim.R
@@ -4,18 +4,21 @@ subcolor_sim <- function(spectra1, spectra2){
   # The two input spectra should be a one-dimentional vector 
   
   all.subcolor.cossim <- c()
+  weight <- c()
   
-  for (j in seq(from = 1, to = 96, by = 16)){
-    this.cossim <- 
-      philentropy::cosine_dist(spectra1[j:(j+15), 1, drop = FALSE], 
-                               spectra2[j:(j+15), 1, drop = FALSE],
-                               testNA = TRUE)
+  for (j in seq(from = 1, to = 96, by = 16)) {
+    s1 <- spectra1[j:(j+15), 1, drop = FALSE]
+    s2 <- spectra2[j:(j+15), 1, drop = FALSE]
     
-
-    all.subcolor.cossim <- c(all.subcolor.cossim, this.cossim)
+    
+    all.subcolor.cossim <- c(all.subcolor.cossim,
+                             philentropy::cosine_dist(s1, s2, testNA = TRUE))
+    weight <- c(weight, sum(s1, s2))
+    
   }
   
   names(all.subcolor.cossim) <- c("C>A","C>G","C>T","T>A","T>C","T>G")
   
-  return(all.subcolor.cossim)
+  return(list(by.color     = all.subcolor.cossim, 
+              weighted.avg = weighted.mean(all.subcolor.cossim, weight)))
 }
